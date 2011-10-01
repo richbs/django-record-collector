f0a329443723f17fec8dcedeeda6389676473a31
Richard Barrett-Small <rickbs@gmail.com>
2011-10-01 12:58:03 +0100

    Basic models

--- shelf/models.py
+++ shelf/models.py
@@ -1,7 +1,65 @@
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    """
+    A recording venue or live gig
+    e.g. Carnegie Hall, Abbey Road Studios
+    """
+    name = models.CharField(blank=False, max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
 
-class Foo(models.Model):
+    def __unicode__(self):
+        return u"%s" % (self.name)
 
-	name = models.CharField(max_length=10)
+
+class Performance(models.Model):
+    """
+    A performance of a work given at a particular date and time
+    Could be a recording date
+    """
+    year = models.IntegerField(blank=True)
+    date = models.DateField()
+    venue = models.ForeignKey(Venue)
+
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __unicode__(self):
+        return u"Performance"
+
+
+class Work(models.Model):
+    """
+    Either a classical opus or a track on an album
+    """
+
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __unicode__(self):
+        return u"Track"
+
+
+class Performer(models.Model):
+    """
+    
+    """
+    
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __unicode__(self):
+        return u"Artist"
+
+class Album(models.Model):
+
+    name = models.CharField(max_length=255)
+    performances = models.ManyToManyField(Performance)
