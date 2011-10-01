47d3803b49280b2705269fdb86610b4450f76ce8
Richard Barrett-Small <rickbs@gmail.com>
2011-10-01 22:22:58 +0100

    Refining models

--- shelf/models.py
+++ shelf/models.py
@@ -6,6 +6,7 @@ class Venue(models.Model):
     e.g. Carnegie Hall, Abbey Road Studios
     """
     name = models.CharField(blank=False, max_length=255)
+    slug = models.SlugField(max_length=255)
     latitude = models.FloatField()
     longitude = models.FloatField()
     
@@ -17,6 +18,40 @@ class Venue(models.Model):
         return u"%s" % (self.name)
 
 
+class Artist(models.Model):
+    """
+    Class for composers, artists, critics
+    Duke Ellington is composer and performer
+    """
+    name = models.CharField(blank=True, max_length=255)
+    slug = models.SlugField(max_length=255)
+    surname = models.CharField(blank=True, max_length=255)
+    given_name = models.CharField(blank=True, max_length=255)
+        
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __unicode__(self):
+        return u"%s" % (name)
+
+class Work(models.Model):
+    """
+    Either a classical opus or a track on an album
+    """
+    title = models.CharField(blank=False, max_length=255)
+    slug = models.SlugField(max_length=255)
+    opus_number = models.CharField(blank=True, max_length=10)
+    composers = models.ManyToManyField(Artist, through="Role")
+    year_composed = models.IntegerField(blank=True, null=True)
+    class Admin:
+        list_display = ('',)
+        search_fields = ('',)
+
+    def __unicode__(self):
+        return u"Track"
+
+    
 class Performance(models.Model):
     """
     A performance of a work given at a particular date and time
@@ -25,41 +60,28 @@ class Performance(models.Model):
     year = models.IntegerField(blank=True)
     date = models.DateField()
     venue = models.ForeignKey(Venue)
+    work = models.ForeignKey(Work)
+    performers = models.ManyToManyField(Artist, through="Role")
 
     class Admin:
         list_display = ('',)
         search_fields = ('',)
 
     def __unicode__(self):
-        return u"Performance"
+        return u"%s %d" % (self.work, self.year)
 
-
-class Work(models.Model):
-    """
-    Either a classical opus or a track on an album
-    """
-
-    class Admin:
-        list_display = ('',)
-        search_fields = ('',)
-
-    def __unicode__(self):
-        return u"Track"
-
-
-class Performer(models.Model):
-    """
-    
-    """
-    
-    class Admin:
-        list_display = ('',)
-        search_fields = ('',)
-
-    def __unicode__(self):
-        return u"Artist"
+class Role(models.Model):
+    artist = models.ForeignKey(Artist)
+    work = models.ForeignKey(Work)
+    performance = models.ForeignKey(Performance)
+    composer = models.BooleanField(default=False)
+    performer = models.BooleanField(default=True)
 
 class Album(models.Model):
 
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
     performances = models.ManyToManyField(Performance)
+    release_date = models.DateField()
+    year = models.IntegerField(blank=True, null=True)
+    
