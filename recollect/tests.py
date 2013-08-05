cff2f6fd963067999939646a455ff8221b62026f
Richard Barrett-Small <rickbs@gmail.com>
2013-08-05 22:11:07 +0100

    Some extra test goodness.

--- recollect/tests.py
+++ recollect/tests.py
@@ -1,20 +1,31 @@
 from django.test import TestCase
-from recollect.models import Album
+from recollect.models import Album, ClassicalAlbum, PopularAlbum
 
 
 class RecollectTest(TestCase):
 
     def setUp(self):
+
         album = Album()
-        album.name = "Harvest Moons"
-        album.year = 1990
+        album.name = "Harvest Moon"
+        album.year = 1992
         album.save()
+
+        cl = ClassicalAlbum()
+        cl.name = "Mahler: Symphony No. 2 \"Resurrection\""
+        cl.year = 1976
+        cl.save()
+
+        pop = PopularAlbum()
+        pop.name = "Help!"
+        pop.year = 1965
+        pop.save()
 
     def test_home(self):
 
         response = self.client.get('/')
         self.assertContains(response, 'Harvest', 1, 200)
-        self.assertContains(response, '<li', 1, 200)
+        self.assertContains(response, '<li', 3, 200)
 
 #     def test_albums(self):
 #
